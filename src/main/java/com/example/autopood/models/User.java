1c08ae1e8b40301fa1a080f34b8674f676d1f007
Karel Nagel <karel789@hotmail.com>
2021-04-17 23:36:14 +0300

    removed user id autogenerate

--- src/main/java/com/example/autopood/models/User.java
+++ src/main/java/com/example/autopood/models/User.java
@@ -6,68 +6,81 @@ import java.util.Set;
 @Entity
 public class User
 {
- @Id @GeneratedValue(strategy = GenerationType.AUTO) private Long id;
+    @Id
+    private Long id;
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Kuulutus> soovitatud;
     @OneToMany(fetch = FetchType.EAGER)
     private Set<Parameter> parameterList;
     private String eesnimi;
     private String perekonnanimi;
-    private String lastAction="";
+    private String lastAction = "";
 
-    public User() {
+    public User()
+    {
     }
-
 
 
     @Override
-    public String toString(){
-        return "Kasutaja ID: "+id+"\n"+
-                "Nimi: "+"\n";
+    public String toString()
+    {
+        return "Kasutaja ID: " + id + "\n" +
+                "Nimi: " + "\n";
     }
 
-    public String getEesnimi() {
+    public String getEesnimi()
+    {
         return eesnimi;
     }
 
-    public void setEesnimi(String eesnimi) {
+    public void setEesnimi(String eesnimi)
+    {
         this.eesnimi = eesnimi;
     }
 
-    public String getPerekonnanimi() {
+    public String getPerekonnanimi()
+    {
         return perekonnanimi;
     }
 
-    public void setPerekonnanimi(String perekonnanimi) {
+    public void setPerekonnanimi(String perekonnanimi)
+    {
         this.perekonnanimi = perekonnanimi;
     }
 
-    public Set<Parameter> getParameters() {
+    public Set<Parameter> getParameters()
+    {
         return parameterList;
     }
 
-    public void addParameters(Parameter parameters) {
+    public void addParameters(Parameter parameters)
+    {
         parameterList.add(parameters);
     }
 
-    public void removeParameters(Parameter parameters) {
+    public void removeParameters(Parameter parameters)
+    {
         parameterList.remove(parameters);
     }
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
-    public String getLastAction() {
+    public String getLastAction()
+    {
         return lastAction;
     }
 
-    public void setLastAction(String s) {
+    public void setLastAction(String s)
+    {
         this.lastAction = s;
     }
 }
